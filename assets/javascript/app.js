3767093baf521c96d96ee4329ca9743b6766a642
Jordan Larios <jolarios96@gmail.com>
2018-11-27 19:07:49 -0800

    moved current firebase code into main app.js

---
+++ assets/javascript/app.js
@@ -0,0 +1,13 @@
+// firebase initialization
+var config = {
+    apiKey: "AIzaSyDzixpXHU87uZ_gbAfy6YP3btwUVek9rWw",
+    authDomain: "project-furr.firebaseapp.com",
+    databaseURL: "https://project-furr.firebaseio.com",
+    projectId: "project-furr",
+    storageBucket: "project-furr.appspot.com",
+    messagingSenderId: "654872510123"
+};
+firebase.initializeApp(config);
+
+
+var storage = firebase.storage();
--- assets/javascript/firebase.js
+++
@@ -1,8 +0,0 @@
-// database reference
-var storage = firebase.storage();
-
-// add storage for user data
-
-
-
-// firebase test code below ::
