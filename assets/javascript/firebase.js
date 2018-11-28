f66b397f2585cb898a1edda0b289e5cd6b00ca96
Jordan Larios <jolarios96@gmail.com>
2018-11-27 22:47:29 -0800

    got a basic email registration working

--- assets/javascript/firebase.js
+++ assets/javascript/firebase.js
@@ -1,14 +1,3 @@
-// firebase initialization
-var config = {
-    apiKey: "AIzaSyDzixpXHU87uZ_gbAfy6YP3btwUVek9rWw",
-    authDomain: "project-furr.firebaseapp.com",
-    databaseURL: "https://project-furr.firebaseio.com",
-    projectId: "project-furr",
-    storageBucket: "project-furr.appspot.com",
-    messagingSenderId: "654872510123"
-};
-firebase.initializeApp(config);
-
 // database reference
 var storage = firebase.storage();
 
